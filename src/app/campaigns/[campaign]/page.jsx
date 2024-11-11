a0b22cec517d005ea6052d43a899331269deea2b
jerald-devOfficial <spaueOfficial@gmail.com>
2024-11-12 02:15:45 +0800

    feat: fetch specific campaign instance and display each details in cards with CampaignDetails card

--- src/app/campaigns/[campaign]/page.jsx
+++ src/app/campaigns/[campaign]/page.jsx
@@ -1,17 +1,74 @@
 import PropTypes from "prop-types";
+import Campaign from "@/campaign";
+import CampaignDetails from "@/components/CampaignDetails";
+import web3 from "web3";
+import ContributeForm from "@/components/ContributeForm";
 
-const Campaign = async ({ params }) => {
+const CampaignShow = async ({ params }) => {
   const { campaign } = await params;
+
+  const campaignInstance = Campaign(campaign);
+  const summary = await campaignInstance.methods.getSummary().call();
+
+  const formattedSummary = {
+    minimumContribution: summary[0],
+    balance: summary[1],
+    requestsCount: summary[2],
+    approversCount: summary[3],
+    manager: summary[4],
+  };
+
+  const campaignDetails = [
+    {
+      header: formattedSummary.manager,
+      meta: "Address of Manager",
+      description:
+        "The manager created this campaign and can create requests to withdraw money.",
+    },
+    {
+      header: formattedSummary.minimumContribution,
+      meta: "Minimum Contribution (wei)",
+      description: "The minimum amount of money needed to become an approver.",
+    },
+    {
+      header: formattedSummary.requestsCount,
+      meta: "Number of Requests",
+      description:
+        "A request tries to withdraw money from the contract. Requests must be approved by the approvers.",
+    },
+    {
+      header: formattedSummary.approversCount,
+      meta: "Number of Approvers",
+      description:
+        "Number of people who have already donated to this campaign.",
+    },
+
+    {
+      header: web3.utils.fromWei(formattedSummary.balance, "ether"),
+      meta: "Campaign Balance (ether)",
+      description:
+        "The balance is how much money this campaign has left to spend.",
+    },
+  ];
+
   return (
-    <div className="flex flex-col">
-      <h1>{campaign}</h1>
-    </div>
+    <main className="flex flex-col gap-y-2">
+      <h3 className="text-xl font-bold ml-6">Campaign Details</h3>
+      <div className="grid grid-cols-5 gap-x-4">
+        <div className="col-span-4 grid grid-cols-2 gap-4 w-full">
+          {campaignDetails.map((detail) => (
+            <CampaignDetails key={detail.meta} {...detail} />
+          ))}
+        </div>
+        <ContributeForm />
+      </div>
+    </main>
   );
 };
 
-export default Campaign;
+export default CampaignShow;
 
-Campaign.propTypes = {
+CampaignShow.propTypes = {
   params: PropTypes.shape({
     campaign: PropTypes.string.isRequired,
   }).isRequired,
