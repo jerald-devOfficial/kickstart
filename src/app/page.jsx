c8ed22e58f6ad5a86566bf870658ffa051dd8441
jerald-devOfficial <spaueOfficial@gmail.com>
2024-11-12 02:11:14 +0800

    feat: update root app.js styling with better grid display

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,22 +8,26 @@ const Home = async () => {
   const campaigns = await factory.methods.getDeployedCampaigns().call();
 
   return (
-    <div className="flex flex-col flex-grow">
-      <div className="w-4/5 flex flex-col gap-y-5 items-start">
+    <main className="flex flex-col">
+      <div className="grid grid-cols-4 gap-4">
         <Suspense fallback={<div>Loading...</div>}>
-          {campaigns.map((campaign) => (
-            <Campaigns key={campaign} campaign={campaign} />
-          ))}
+          <div className="col-span-3 flex flex-col gap-4">
+            {campaigns.map((campaign) => (
+              <Campaigns key={campaign} campaign={campaign} />
+            ))}
+          </div>
         </Suspense>
-        <Link
-          href="/campaigns/new"
-          className="flex gap-x-2 items-center justify-center bg-blue-500 text-white px-4 py-2 rounded-lg"
-        >
-          <LuPlusCircle size={20} />
-          <span className="text-sm">Create Campaign</span>
-        </Link>
+        <div className="col-span-1">
+          <Link
+            href="/campaigns/new"
+            className="flex w-full gap-x-2 items-center justify-center bg-blue-500 text-white px-4 py-2 rounded-lg"
+          >
+            <LuPlusCircle size={20} />
+            <span className="text-sm">Create Campaign</span>
+          </Link>
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
