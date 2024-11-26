11e825f9f4d304b3a3dea075073d8ee938aa722d
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-11-27 00:18:58 +0800

    chore: add props type for campaign props in ContributeForm

--- src/components/ContributeForm.jsx
+++ src/components/ContributeForm.jsx
@@ -1,92 +1,96 @@
-"use client";
-import Campaign from "@/campaign"
-import Message from "@/components/Message"
-import { Button } from "@/components/ui/button"
+'use client'
+import Campaign from '@/campaign'
+import Message from '@/components/Message'
+import { Button } from '@/components/ui/button'
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import web3 from "@/web3"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useMutation } from "@tanstack/react-query"
-import { Loader2 } from "lucide-react"
-import { useRouter } from "next/navigation"
-import { useReducer } from "react"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import web3 from '@/web3'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import PropTypes from 'prop-types'
+import { useReducer } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 const formSchema = z.object({
-  value: z.number().min(1, "Value must be a positive number"),
-});
+  value: z.number().min(1, 'Value must be a positive number')
+})
 
-const ContributeForm =  ({campaign}) => {
-  const { push } = useRouter();
-  const reducer = (state, action) => ({ ...state, ...action });
+const ContributeForm = ({ campaign }) => {
+  const { push } = useRouter()
+  const reducer = (state, action) => ({ ...state, ...action })
   const initialState = {
-    buttonText: "Contribute!",
+    buttonText: 'Contribute!',
     success: null,
-    error: null,
-  };
+    error: null
+  }
 
   const [{ buttonText, error, success }, dispatch] = useReducer(
     reducer,
     initialState
-  );
+  )
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      value: 100,
-    },
-  });
+      value: 100
+    }
+  })
 
   // Define the mutation
   const { mutate, isPending } = useMutation({
     mutationFn: async (data) => {
-      dispatch({ buttonText: "Sending...", error: null, success: null });
+      dispatch({ buttonText: 'Sending...', error: null, success: null })
       const currentCampaign = Campaign(campaign)
-      const accounts = await web3.eth.getAccounts();
+      const accounts = await web3.eth.getAccounts()
       return currentCampaign.methods.contribute().send({
         from: accounts[0],
-        value: web3.utils.toWei(data.value, "ether"),
-      });
+        value: web3.utils.toWei(data.value, 'ether')
+      })
     },
     onSuccess: () => {
-      dispatch({ buttonText: "Sent!", success: 'Successfully sent ether to the campaign.' });
-      push("/");
+      dispatch({
+        buttonText: 'Sent!',
+        success: 'Successfully sent ether to the campaign.'
+      })
+      push('/')
     },
     onError: ({ message }) => {
-      dispatch({ error: message, buttonText: "Try again!" });
-    },
-  });
+      dispatch({ error: message, buttonText: 'Try again!' })
+    }
+  })
 
   const onSubmit = (values) => {
-    mutate(values);
-  };
+    mutate(values)
+  }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
         <FormField
           control={form.control}
-          name="value"
+          name='value'
           render={({ field }) => (
             <FormItem>
-              <FormLabel className="font-bold">Amount to Contribute</FormLabel>
-              <div className="flex items-center w-full">
+              <FormLabel className='font-bold'>Amount to Contribute</FormLabel>
+              <div className='flex items-center w-full'>
                 <FormControl>
                   <Input
-                    type="number"
-                    placeholder="100"
+                    type='number'
+                    placeholder='100'
                     {...field}
-                    className="rounded-r-none focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-blue-200"
+                    className='rounded-r-none focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-blue-200'
                   />
                 </FormControl>
-                <span className="h-10 flex items-center bg-gray-200 px-2 rounded-l-none border-y border-r rounded-r-md">
+                <span className='h-10 flex items-center bg-gray-200 px-2 rounded-l-none border-y border-r rounded-r-md'>
                   ether
                 </span>
               </div>
@@ -94,15 +98,19 @@ const ContributeForm =  ({campaign}) => {
             </FormItem>
           )}
         />
-        <Button type="submit" className="bg-blue-600 text-white font-medium">
-          {isPending && <Loader2 className="w-4 h-4 animate-spin" />}
+        <Button type='submit' className='bg-blue-600 text-white font-medium'>
+          {isPending && <Loader2 className='w-4 h-4 animate-spin' />}
           {buttonText}
         </Button>
-        {success && <Message message={success} type="success" />}
-        {error && <Message message={error} type="error" />}
+        {success && <Message message={success} type='success' />}
+        {error && <Message message={error} type='error' />}
       </form>
     </Form>
-  );
-};
+  )
+}
 
-export default ContributeForm;
+ContributeForm.propTypes = {
+  campaign: PropTypes.object.isRequired
+}
+
+export default ContributeForm
