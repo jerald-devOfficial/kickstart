b8f301c973035cb1979a8ebb6a6063a2d3d09768
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-11-29 23:40:04 +0800

    feat: use table to display columns and requests in RequestPage

--- src/app/campaigns/[address]/requests/page.jsx
+++ src/app/campaigns/[address]/requests/page.jsx
@@ -1,6 +1,25 @@
 import Campaign from '@/campaign'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
+
+const columns = [
+  'ID',
+  'Description',
+  'Amount',
+  'Recipient',
+  'Approval Count',
+  'Approve',
+  'Finalize'
+]
 
 const RequestPage = async ({ params }) => {
   const { address } = await params
@@ -13,11 +32,32 @@ const RequestPage = async ({ params }) => {
       .map((element, index) => campaignInstance.methods.requests(index).call())
   )
 
-  console.log('requests', requests)
-
   return (
     <main className='flex flex-col gap-y-5'>
       <h3 className='text-xl font-bold ml-6'>Requests</h3>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow className='bg-gray-100 rounded-2xl border-gray-300 border'>
+            {columns.map((column) => (
+              <TableHead key={column}>{column}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {requests.map((request, index) => (
+            <TableRow key={request.id}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>{request.description}</TableCell>
+              <TableCell>{request.value.toString()}</TableCell>
+              <TableCell>{request.recipient}</TableCell>
+              <TableCell>{request.approvalCount}</TableCell>
+              <TableCell>{request.approve}</TableCell>
+              <TableCell>{request.finalize}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
       <div className='block'>
         <Link
           className='bg-blue-500 text-white px-4 py-2 rounded-md font-semibold'
