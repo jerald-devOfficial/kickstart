23819fa47b5e008edb2f06b832eb25fbf2259e08
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-11-29 22:45:41 +0800

    feat: add back button on CreateRequest page

--- src/app/campaigns/[campaign]/requests/new/page.jsx
+++ src/app/campaigns/[campaign]/requests/new/page.jsx
@@ -1,10 +1,18 @@
 import CreateRequestForm from '@/components/CreateRequestForm'
+import { MoveLeft } from 'lucide-react'
+import Link from 'next/link'
 import PropTypes from 'prop-types'
 
 const CreateRequest = async ({ params }) => {
   const { campaign } = await params
   return (
     <main className='flex flex-col gap-y-5'>
+      <Link
+        className='text-blue-600 flex gap-x-1 items-center'
+        href={`/campaigns/${campaign}/requests`}
+      >
+        <MoveLeft size={20} /> <span>Back</span>
+      </Link>
       <h3 className='text-xl font-bold'>Create a Request</h3>
       <CreateRequestForm campaign={campaign} />
     </main>
