d5741d8549e944a5a844e3e15f2e0e8f885f2298
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-12-02 00:20:12 +0800

    feat: update deploy.js, use evm and abi instead of using interface and bytcode directly

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,15 +1,11 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Web3 = require('web3').default;
-const compiledFactory = require('./build/CampaignFactory.json');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+require('dotenv').config();
 
-// showing this but not actually working
-const dotenv = require('dotenv');
-dotenv.config({ path: ".env.local" });
-
-// unfortunatley, this is not working if you use .env variables, so please use the hardcoded values below
 const provider = new HDWalletProvider(
-  process.env.METAMAST_MNEMONIC,
-  `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+  "YOUR_MNEMONIC",
+  `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`
 );
 
 const web3 = new Web3(provider);
@@ -19,11 +15,9 @@ const deploy = async () => {
 
   console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web3.eth.Contract(
-    JSON.parse(compiledFactory.interface)
-  )
-    .deploy({ data: compiledFactory.bytecode })
-    .send({ gas: "1000000", from: accounts[0] });
+  const result = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: compiledFactory.evm.bytecode.object })
+    .send({ gas: "1400000", from: accounts[0] });
 
   console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
