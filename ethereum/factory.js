8e7b44e795b25f61ad2e954f037f9e0b6bb47874
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-12-02 00:22:28 +0800

    feat: update factory.js from using interface to using abi

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,11 +1,11 @@
-import dotenv from "dotenv";
-import web3 from "./web3";
-import CampaignFactory from "./build/CampaignFactory.json";
+import dotenv from "dotenv"
+import CampaignFactory from "./build/CampaignFactory.json"
+import web3 from "./web3"
 
 dotenv.config({ path: ".env.local" });
 
 const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
+  JSON.parse(CampaignFactory.abi),
   process.env.CONTRACT_ADDRESS
 );
 
